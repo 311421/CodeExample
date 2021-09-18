2f478b8d69a8348d092f3ccaf5444147fb4986d9
311421 <tegrasgt@gmail.com>
2021-09-18 21:46:07 +0300

    Student constructor

--- Isu/Objects/Student.cs
+++ Isu/Objects/Student.cs
@@ -2,5 +2,13 @@
 {
     public class Student
     {
+        private string _name;
+        private Group _studentGroup;
+
+        public Student(string name, Group group)
+        {
+            _name = name;
+            _studentGroup = group;
+        }
     }
 }
