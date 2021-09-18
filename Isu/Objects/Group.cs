d23464f959ab5fde4e128f31d346215af750d3e2
311421 <tegrasgt@gmail.com>
2021-09-18 21:45:07 +0300

    feat: added AddStudent and constructor

--- Isu/Objects/Group.cs
+++ Isu/Objects/Group.cs
@@ -1,6 +1,20 @@
-﻿namespace Isu.Objects
+﻿using System.Collections.Generic;
+
+namespace Isu.Objects
 {
     public class Group
     {
+        private List<Student> _students = new List<Student>();
+        private string _groupName;
+
+        public Group(string name)
+        {
+            _groupName = name;
+        }
+
+        public void AddStudent(string name)
+        {
+            _students.Add(new Student(name, this));
+        }
     }
 }
